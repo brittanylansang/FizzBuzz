8d848de1329dc2249097bf4883e6b7149346364a
Brittany <brittany.lansang@gmail.com>
2023-09-14 21:46:35 -0400

    refactored to extract helper method

---
+++ src/Multiples.java
@@ -0,0 +1,13 @@
+public class Multiples {
+
+    public static void main(String[] args) {
+        int multiples = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                multiples++;
+            }
+        }
+        System.out.print(multiples);
+    }
+}
